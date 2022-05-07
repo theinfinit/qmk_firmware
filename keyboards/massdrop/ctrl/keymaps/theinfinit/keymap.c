22cb35f3a843639af3afd823b48c609da91583fb
Konrad Serafin <konradserafin.koszalin@gmail.com>
2022-05-07 21:29:05 +0200

    massdrop/ctrl `v0.3.3` minor layout improvements (more in desc)
    - changed order of `number` and `symbol` layers
    - moved replaced `RSHIFT` with `ESC`

--- keyboards/massdrop/ctrl/keymaps/theinfinit/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/theinfinit/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5, KC_F6, KC_F7,        KC_F8,        KC_F9,        KC_F10,          KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS, \
         KC_GRV,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,  KC_6,  KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, \
-        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,  KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,            KC_ESC,  KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, \
+        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,  KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, \
         MO(6),   LALT_T(KC_A), LSFT_T(KC_S), LCTL_T(KC_D), LGUI_T(KC_F), KC_G,  KC_H,  RGUI_T(KC_J), RCTL_T(KC_K), RSFT_T(KC_L), RALT_T(KC_SCLN), LT(5,KC_QUOT), KC_ENT, \
-        KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,  KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,                            KC_UP, \
-        KC_LCTL, KC_LGUI, MO(3),                                         KC_SPC,                                   MO(4),        MO(2),           KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,  KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_ESC,                             KC_UP, \
+        KC_LCTL, KC_LGUI, MO(4),                                         KC_SPC,                                   MO(3),        MO(2),           KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_7,    KC_8,    KC_9, \
@@ -39,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT(
         _______,      _______,    _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, \
         _______,      KC_F1,      KC_F2,        KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,  _______, _______, _______, \
-        LALT(KC_TAB), KC_1,       KC_2,         KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______,  _______, _______, _______, \
-        _______,      LALT(KC_A), LSFT_T(KC_S), KC_LPRN, KC_RPRN, KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  KC_TILD, _______, \
-        _______,      _______,    _______,      _______, KC_UNDS, _______, KC_EQL,  KC_LCBR, KC_RCBR, _______, _______, _______,                             _______, \
+        LALT(KC_TAB), KC_EXLM,    KC_AT,        KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_UNDS, _______, _______, _______,  _______, _______, _______, \
+        _______,      KC_1,       KC_2,         KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______, _______, \
+        _______,      _______,    _______,      KC_UNDS, _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, _______,                             _______, \
         _______,      _______,    _______,                                 _______,                   _______, _______, _______, _______,           _______, _______, _______ \
     ),
     [4] = LAYOUT(
         _______,      _______,    _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, \
         _______,      KC_F1,      KC_F2,        KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______,  _______, _______, _______, \
-        LALT(KC_TAB), KC_EXLM,    KC_AT,        KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_UNDS, _______, _______, _______,  _______, _______, _______, \
-        _______,      KC_1,       KC_2,         KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______, _______, \
-        _______,      _______,    _______,      KC_UNDS, _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, _______,                             _______, \
+        LALT(KC_TAB), KC_1,       KC_2,         KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______,  _______, _______, _______, \
+        _______,      LALT(KC_A), LSFT_T(KC_S), KC_LPRN, KC_RPRN, KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  KC_TILD, _______, \
+        _______,      _______,    _______,      _______, KC_UNDS, _______, KC_EQL,  KC_LCBR, KC_RCBR, _______, _______, _______,                             _______, \
         _______,      _______,    _______,                                 _______,                   _______, _______, _______, _______,           _______, _______, _______ \
     ),
     [5] = LAYOUT(
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-//    rgb_enabled_flag = true;  // Initially, keyboard RGB is enabled. Change to false config.h initializes RGB disabled.
+rgb_enabled_flag = true;  // Initially, keyboard RGB is enabled. Change to false config.h initializes RGB disabled.
 };
 
 void keyboard_post_init_user(void) { rgb_matrix_enable(); }
